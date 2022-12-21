d6ef16433abba6bcff6b31e2393c40d910f47fbc
muharioadriansyahw@gmail.com <muharioadriansyahw@gmail.com>
2022-12-21 21:31:42 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.home');
+});
+
+Route::get('/detail', function () {
+    return view('pages.detail');
+});
+
+Route::get('/checkout', function () {
+    return view('pages.checkout');
+});
+
+Route::get('/checkout/success', function () {
+    return view('pages.success');
+});
+
+Route::get('admin', [DashboardController::class, 'index'])->middleware(['auth','admin'])->name('admin');
+
+Auth::routes(['verify' => true]);
